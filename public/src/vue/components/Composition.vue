91332b08db2d86f5a610a06146077420cd6a0e5c
Louis Eveillard <barlou15+github@gmail.com>
2019-12-11 16:47:51 +0100

    removed column "last modifications" in compositions

--- public/src/vue/components/Composition.vue
+++ public/src/vue/components/Composition.vue
@@ -5,16 +5,16 @@
         <thead>
           <tr>
             <th style>{{ $t('compositions') }}</th>
-            <th>{{ $t('date_modified') }}</th>
+            <!-- <th>{{ $t('date_modified') }}</th> -->
             <th colspan="2">{{ $t('actions') }}</th>
           </tr>
         </thead>
         <transition-group tag="tbody" name="list-complete">
           <tr v-for="w in composition_medias" :key="w.metaFileName">
             <td>{{ w.name }}</td>
-            <td
+            <!-- <td
               :title="$moment(w.date_modified).format('l LTS')"
-            >{{ format_date_to_human(w.date_modified) + ' ' + $moment(w.date_modified).format('HH:mm') }}</td>
+            >{{ format_date_to_human(w.date_modified) + ' ' + $moment(w.date_modified).format('HH:mm') }}</td>-->
             <td>
               <button
                 type="button"
