ffe2b3822426e50485e8f452b7b82672b883fad8
Louis Eveillard <barlou15+github@gmail.com>
2020-11-11 16:10:56 +0100

    fixed tanslations

--- public/src/vue/locale_strings.js
+++ public/src/vue/locale_strings.js
@@ -21,6 +21,8 @@ module.exports = (function () {
       open: "Ouvrir",
       back: "Retour",
       at: "à",
+      edit: "Modifier",
+
       settings: "Règlages",
       new_project: "Nouveau Projet",
       project: "Projet",
@@ -28,6 +30,14 @@ module.exports = (function () {
       onion_skin: "Onion skin",
       keyword: "Mot-clé",
       keywords: "Mot-clés",
+      more_informations: "Plus d’informations",
+      users_connected: "Utilisateurs connectes",
+      channels: "Sujets",
+      users: "Utilisateurs",
+      login: "S’identifier",
+      logout: "Se déconnecter",
+      contributor: "Contributeur.trice",
+      admin: "Adminstrateur.trice",
 
       photo: "photo",
       video: "vidéo",
