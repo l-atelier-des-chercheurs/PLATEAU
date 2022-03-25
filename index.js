15741afa1d69e2fd7ed8c3c2aed4356f167f5073
Louis Eveillard <barlou15+github@gmail.com>
2022-03-25 18:53:54 +0100

    CORE / updated with fixes

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-const path = require("path");
-const packagejson = require("./package.json");
-const base_settings = require("./settings_base.json");
+const path = require('path');
+const packagejson = require('./package.json');
+const base_settings = require('./settings_base.json');
 
 global.appRoot = path.resolve(__dirname);
 global.appInfos = {
   name: packagejson.name,
   productName: packagejson.productName,
-  version: packagejson.version,
+  version: packagejson.version
 };
-
-// public (browserify) or app (vue-cli)
-global.frontEnd = "public";
 
 let settings = base_settings;
 try {
-  const override_settings = require("./settings.json");
+  const override_settings = require('./settings.json');
   Object.assign(settings, override_settings);
-  console.log("INDEX / found override settings.json");
+  console.log('INDEX / found override settings.json');
 } catch (ex) {
-  console.log("INDEX / didn’t find override settings");
+  console.log('INDEX / didn’t find override settings');
 }
 global.settings = settings;
 
-const router = require("./router");
+const router = require('./router');
 
-require("./core/main")({
-  router,
+require('./core/main')({
+  router
 });
